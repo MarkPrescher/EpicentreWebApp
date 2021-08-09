31355d2d4db54cb808037cbe40ec24671c37337c
Mark Prescher <msprescher@gmail.com>
2021-08-09 12:56:17 +0200

    Added Models

---
+++ Data/EpicentreDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Epicentre.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Epicentre.Data
+{
+    public class EpicentreDataContext : DbContext
+    {
+        public EpicentreDataContext(DbContextOptions<EpicentreDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CovidTest> CovidTest { get; set; }
+        public DbSet<CovidVaccination> CovidVaccination { get; set; }
+    }
+}
