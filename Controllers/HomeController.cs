be5bd8edb797f03ed2ee392e04259c0d28ad10b8
Nikita Ramraj <nikkiyearthree@gmail.com>
2021-08-17 14:00:04 +0200

    Layout views for each controller created Created controllers for each menu item

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace Epicentre.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
