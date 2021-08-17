d6c10a5fb58c7635773e66ae755efa3200240f3b
Varshen <varshen240@gmail.com>
2021-08-17 12:49:04 +0200

    Updated the covidTest and covidVaccination models

--- Models/CovidTest.cs
+++ Models/CovidTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,29 @@ namespace Epicentre.Models
 {
     public class CovidTest
     {
+        [Key]
+        public Guid TEST_ID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a test type")]
+        [StringLength(25, ErrorMessage = "Please enter a value with a maximum of 25 characters")]
+        public string TEST_TYPE { get; set; }
+
+        [Required(ErrorMessage = "Please enter a test date")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string TEST_DATE { get; set; }
+
+        [Required(ErrorMessage = "Please input test status")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+
+        public string TEST_STATUS { get; set; }
+
+        [Required(ErrorMessage = "Please input test result")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string TEST_RESULT { get; set; }
+
+        [Required(ErrorMessage = "Please enter user ID")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string USER_ID { get; set; }
+        
     }
 }
--- Models/CovidVaccination.cs
+++ Models/CovidVaccination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,28 @@ namespace Epicentre.Models
 {
     public class CovidVaccination
     {
+        [Key]
+        public Guid VACCINATION_ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a vaccination type")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string VACCINATION_TYPE { get; set; }
+
+        [Required(ErrorMessage ="Please enter a date")]
+        [StringLength(55,ErrorMessage ="Please enter a value with a maximum of 55 characters")]
+        public string VACCINATION_DATE { get; set; }
+
+        [Required(ErrorMessage ="Please enter next date")]
+        [StringLength(55,ErrorMessage ="Please enter a value with a maximum of 55 characters")]
+        public string VACCINATION_NEXT_DATE { get; set; }
+
+        [Required(ErrorMessage = "Please enter status")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string VACCINATION_STATUS { get; set; }
+        
+        [Required(ErrorMessage = "Please enter user id date")]
+        [StringLength(55, ErrorMessage = "Please enter a value with a maximum of 55 characters")]
+        public string USER_ID { get; set; }
+
     }
 }
