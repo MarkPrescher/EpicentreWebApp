854b5a36761bd54a95e6159cc07ea45433ec1131
Mark Prescher <msprescher@gmail.com>
2021-11-09 12:50:27 +0200

    Added error handling page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Epicentre.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the page requested could not be found!";
+                    break;
+
+            }
+            return View("NotFound");
+        }
+    }
+}
